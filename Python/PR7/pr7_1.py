67c30b5702582700c4bb7f60b2566ae1c178d70f
oleksandrkozlovskyi2-ai <oleksandr.kozlovskyi2@gmail.com>
2025-09-22 09:37:12 +0300

    Update pr7_1.py

--- Python/PR7/pr7_1.py
+++ Python/PR7/pr7_1.py
@@ -5,42 +5,39 @@ def Open(file_name, mode):
         print("Файл", file_name, "не вдалося відкрити!")
         return None
     else:
-        print("Файл", file_name, "успішно відкрито!")
         return file
 
 
-# --- а) створення TF10_1.txt ---
 file1_name = "TF10_1.txt"
 file2_name = "TF10_2.txt"
 
+# --- а) створення TF10_1.txt ---
 file_1_w = Open(file1_name, "w")
-if file_1_w is not None:
+if file_1_w:
     file_1_w.write("Python3 is great!\n")
     file_1_w.write("File123 processing test.\n")
     file_1_w.write("Numbers 9876543210 should be removed!\n")
     file_1_w.close()
-    print("Дані успішно записані у TF10_1.txt!\n")
 
 # --- б) читання TF10_1.txt, видалення цифр і запис у TF10_2.txt ---
 file_1_r = Open(file1_name, "r")
 file_2_w = Open(file2_name, "w")
 
-if file_1_r is not None and file_2_w is not None:
+if file_1_r and file_2_w:
     text = file_1_r.read()
-    # Видаляємо всі цифри
-    text_no_digits = ''.join([ch for ch in text if not ch.isdigit()])
+    # видаляємо цифри і символи переводу рядка
+    text_no_digits = ''.join([ch for ch in text if not ch.isdigit() and ch != '\n'])
 
-    # Запис по 10 символів у рядок
+    # запис по 10 символів у рядок
     for i in range(0, len(text_no_digits), 10):
         file_2_w.write(text_no_digits[i:i+10] + "\n")
 
     file_1_r.close()
     file_2_w.close()
-    print("Файл TF10_2.txt сформовано!\n")
 
 # --- в) читання TF10_2.txt і виведення ---
 file_2_r = Open(file2_name, "r")
-if file_2_r is not None:
+if file_2_r:
     print("Вміст TF10_2.txt:")
     for line in file_2_r:
         print(line.strip())
